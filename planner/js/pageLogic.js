a3732b1ca4b6c45440229419f9bf7ca978054016
anon <nonomail@nonomail.com>
2025-02-07 00:45:32 -0300

    bondgear

--- planner/js/pageLogic.js
+++ planner/js/pageLogic.js
@@ -3493,9 +3493,9 @@ function populateCharModal(charId) {
         document.getElementById("input_bond_current").value = charData.current?.bond;
         document.getElementById("input_bond_target").value = charData.target?.bond;
 
+        if (!charData.current.bondgear) charData.current.bondgear = "1";
+        if (!charData.target.bondgear) charData.target.bondgear = "1";
         if (misc_data.bondgear_characters.includes(parseInt(charId))) {
-            if (!charData.current.bondgear) charData.current.bondgear = "1";
-            if (!charData.target.bondgear) charData.target.bondgear = "1";
             document.getElementById("input_bondgear_current").value = charData.current?.bondgear;
             document.getElementById("input_bondgear_target").value = charData.target?.bondgear;
             document.getElementById("bondgear_tablecell_header").style.display = ""
