37097bf7d16efa635e8b72b0aa88556132ff4c2f
anon <nonomail@nonomail.com>
2025-02-07 14:12:15 -0300

    Update pageLogic.js

--- planner/js/pageLogic.js
+++ planner/js/pageLogic.js
@@ -286,8 +286,8 @@ function init() {
         }
 
         for (let i in data.characters) {
-            if (!data.characters[i].current.bondgear || data.characters[i].current.bondgear == "") data.characters[i].current.bondgear = 1
-            if (!data.characters[i].target.bondgear || data.characters[i].target.bondgear == "") data.characters[i].target.bondgear = 1
+            if (!data.characters[i].current.bondgear) data.characters[i].current.bondgear = 1
+            if (!data.characters[i].target.bondgear) data.characters[i].target.bondgear = 1
         }
         // if (!data.level_cap) {
         //     data.level_cap = 90;
