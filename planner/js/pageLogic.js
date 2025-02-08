abd08f4811986435413b05dd6800ba46d2cca4b2
anon <nonomail@nonomail.com>
2025-02-08 18:12:22 -0300

    a

--- planner/js/pageLogic.js
+++ planner/js/pageLogic.js
@@ -399,8 +399,8 @@ function init() {
         ["Totem", "Baghdad", "Fleece", "Okiku", "Colgante", "Atlantis", "RomanDice", "Quimbaya", "Rocket", "Mystery"], 9,
         tableNavigation, document.getElementById("table-parent-3"), true, "resource", "icons/Artifact/", [], "artifact-");
     
-    createTable("workbook-table", ["3_potentialhealpower","3_potentialattack","3_potentialmaxhp"] , 0,
-        ["Workbook"], 0,
+    createTable("workbook-table", ["3_potentialhealpower","3_potentialattack","3_potentialmaxhp"] , 4,
+        ["Workbook"], 1,
         tableNavigation, document.getElementById("table-parent-7"), true, "resource", "icons/LimitBreak/", [], "workbook-");
 
     let gearNavigation = [];
