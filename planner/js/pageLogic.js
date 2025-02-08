d5c4d81338ca3fe40fb85f1d3ed6e188eff05ca9
anon <nonomail@nonomail.com>
2025-02-07 23:35:44 -0300

    a

--- planner/js/pageLogic.js
+++ planner/js/pageLogic.js
@@ -5647,7 +5647,7 @@ function calcSkillCost(characterObj, skill, current, target, matDict) {
     else if (skill == "bondgear") {
         if (characterObj.Gear) skillMaterials = characterObj.Gear.TierUpMaterial;
         else { skillMaterials = [] }
-        skillMaterialAmounts = misc_data.bondgearMaterialAmount;
+        skillMaterialAmounts = characterObj.Gear.TierUpMaterialAmount;
         skillType = "bondgear";
     }
     else {
