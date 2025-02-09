103510a0d55dd83652991fb7c1cc06d0eca2a1bf
anon <nonomail@nonomail.com>
2025-02-08 22:24:45 -0300

    Update pageLogic.js

--- planner/js/pageLogic.js
+++ planner/js/pageLogic.js
@@ -408,8 +408,10 @@ function init() {
     let usedGifts = []
     for (let i in charlist) {
         console.log(charlist[i])
-        if (charlist[i].Gear.TierUpMaterial) {
-            usedGifts.push(charlist[i].Gear.TierUpMaterial[0])
+        if (charlist[i].Gear) {
+            if (charlist[i].Gear.TierUpMaterial) {
+                usedGifts.push(charlist[i].Gear.TierUpMaterial[0])
+            }
         }
     }
     for (let i = 0 ; i < usedGifts.lenght ; i++) {
