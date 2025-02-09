0bcdc566fe49ba0b1c80a80eef57d01bd17be0c2
anon <nonomail@nonomail.com>
2025-02-09 17:39:21 -0300

    a

--- planner/js/plannerFast.js
+++ planner/js/plannerFast.js
@@ -446,6 +446,7 @@ function updateInfoDisplay(charId, idInject, charData) {
     let bondgear_characters = [ 10066, 10000, 10028, 20009, 13001, 10047, 10001, 20021, 23007, 23003, 20000, 20005, 10004, 10022, 10036, 10005, 10023, 10009, 16006, 13007, 20015, 10065, 16002, 23008, 20003, 10034, 10041, 10038, 10039, 10008, 13004, 20006, 13008, 10010, 23006, 10025, 10012, 16003, 10062, 10013, 23004, 10033, 26005, 13010 ]
     console.log(parseInt(charId), bondgear_characters.includes(parseInt(charId)))
     if ( bondgear_characters.includes(charId) && charData.current?.bondgear>0) {
+        document.getElementById("bondgearPortraitHeart"+ charId).style.display = ""
         document.getElementById(charId + idInject + "-bondgear-current").innerText = "T"+ charData.current?.bondgear;
         if (charData.current?.bondgear != charData.target?.bondgear) {
             document.getElementById(charId + idInject + "-bondgear-target").innerText = "T"+ charData.target?.bondgear;
