2f2508d66f622bf017e54978618d56b11ab38bf2
anon <nonomail@nonomail.com>
2025-02-08 22:32:34 -0300

    a

--- planner/js/pageLogic.js
+++ planner/js/pageLogic.js
@@ -416,7 +416,7 @@ function init() {
     let giftsRows = 0
     //giftsList[0] = []
     for (let i = 0 ; i < usedGifts.length ; i++) {
-        giftsList.push(matLookup.get(i).replace("favor_","")) //If it works, it works.
+        giftsList.push(matLookup.get(usedGifts[i]).replace("favor_","")) //If it works, it works desu...
         /*if ( i != 5000 && i%10 == 0) {
             giftsRows += 1
             giftsList[giftsRows] = []
