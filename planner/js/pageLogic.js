21ed49fba33a70bc58eeafa729eb514be04e15ae
anon <nonomail@nonomail.com>
2025-02-12 14:13:59 -0300

    Update pageLogic.js

--- planner/js/pageLogic.js
+++ planner/js/pageLogic.js
@@ -418,7 +418,7 @@ function init() {
     }
 
     let gearNavigation = [];
-    createTable("gear-table", ["T10", "T9", "T8", "T7", "T6", "T5", "T4", "T3", "T2"], 0, ["Hat", "Gloves", "Shoes", "Bag", "Badge", "Hairpin", "Charm", "Watch", "Necklace"],
+    createTable("gear-table", ["T2", "T3", "T4", "T5", "T6", "T7", "T8", "T9", "T10", ], 0, ["Hat", "Gloves", "Shoes", "Bag", "Badge", "Hairpin", "Charm", "Watch", "Necklace"],
         0, gearNavigation, document.getElementById('table-parent-4'), false, "gear", "icons/Gear/", [], "gear-");
 
     let navObj = {};
