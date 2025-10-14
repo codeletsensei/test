3d4f3c8a5dbcac757d7503756b23f267accd54c4
anon <nonomail@nonomail.com>
2025-10-14 01:08:04 -0300

    a

--- planner/js/common.js
+++ planner/js/common.js
@@ -665,3 +665,17 @@ function basicAlert(alertText) {
         timer: 1500
     })
 }
+
+function leaveSite(){
+    Swal.fire({
+        icon: "warning",
+        title: "You're about to access another website...",
+        html: "The website linked seems useful, but its code is not open source, nor do I know of its background. So access it at your own risk."
+        showCancelButton: true,
+        confirmButtonText: "Proceed anyways.",
+    })
+    .then((result) => {
+      if (result.isConfirmed) return 1
+      else if (result.isDenied) return null
+    })
+}
